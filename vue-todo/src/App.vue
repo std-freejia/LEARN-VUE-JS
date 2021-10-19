843db1a12d8f0a41c4b0168213bc8e11faed109e
std-freejia <wtfreejia@gmail.com>
2021-10-19 20:41:31 +0900

    TODO App 프로젝트 생성하고 컴포넌트 등록

---
+++ vue-todo/src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <div id="app">
+    <TodoHeader></TodoHeader>
+    <TodoInput></TodoInput>
+    <TodoList></TodoList>
+    <TodoFooter></TodoFooter>
+  </div>
+</template>
+
+<script>
+import TodoFooter from './components/TodoFooter.vue'
+import TodoHeader from './components/TodoHeader.vue'
+import TodoInput from './components/TodoInput.vue'
+import TodoList from './components/TodoList.vue'
+
+export default {
+  components: { TodoHeader, TodoInput, TodoList, TodoFooter },
+}
+
+</script>
+
+<style>
+
+</style>
